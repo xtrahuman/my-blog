ed2d87ae5fcc306a74e46a0db00466d43625350e
xtrahuman <okpara_tochukwu@yahoo.com>
2022-02-22 16:10:50 +0100

    update post association, made all update counters private, fixed errors in last_five_comments method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ApplicationRecord
+  after_save :update_comments_counter
+
   belongs_to :post
   belongs_to :user
+
+  private
 
   def update_comments_counter
     post.increment!(:comments_counter)
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,10 @@
 class Like < ApplicationRecord
+  after_save :update_likes_counter
+
   belongs_to :post
   belongs_to :user
+
+  private
 
   def update_likes_counter
     post.increment!(:likes_counter)
