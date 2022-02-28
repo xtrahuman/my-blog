cf13ad0405f9cfdb312fa99c789b6557d9f55786
xtrahuman <okpara_tochukwu@yahoo.com>
2022-02-28 15:00:23 +0100

    add devise and setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  # validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
   def last_three_posts
     posts.order(created_at: :desc).limit(3)
   end
