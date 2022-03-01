64a57fd7d25e802735014f29990308e3dcc66d1f
xtrahuman <okpara_tochukwu@yahoo.com>
2022-03-01 15:29:22 +0100

    fixed email confirmable

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index create new show]
   end
