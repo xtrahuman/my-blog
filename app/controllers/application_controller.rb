1227d415120b3f392862e29b4f40ad76fd78b0a4
xtrahuman <okpara_tochukwu@yahoo.com>
2022-03-01 13:49:34 +0100

    removed errors from controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  layout "application"
   protect_from_forgery prepend: true
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
