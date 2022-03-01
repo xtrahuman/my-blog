e52b3259ab8e1bceee85a6ce162de0f687257870
xtrahuman <okpara_tochukwu@yahoo.com>
2022-03-01 12:37:03 +0100

    reset password and email confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :user
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index create new show]
   end
